8afff5c04408d8ebcc27b158a30885c9b0bc8115
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-13 18:47:28 -0300

    slugfy

--- src/main/java/br/com/backend/challenger/demo/domain/PlaceService.java
+++ src/main/java/br/com/backend/challenger/demo/domain/PlaceService.java
@@ -1,19 +1,22 @@
 package br.com.backend.challenger.demo.domain;
 
 import br.com.backend.challenger.demo.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
   private PlaceRepository placeRepository;
+  private Slugify slg;
 
   public PlaceService(PlaceRepository placeRepository) {
     this.placeRepository = placeRepository;
+    this.slg = Slugify.builder().build();
   }
 
   public Mono<Place> create(PlaceRequest placeRequest) {
     var place = new Place(
             null, placeRequest.name(),
-            placeRequest.slug(),
+            slg.slugify(placeRequest.name()),
             placeRequest.state(),
             placeRequest.createdAt(),
             placeRequest.updatedAt()
